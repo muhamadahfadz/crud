c3b5eb7afba824e3849c2c58b9fdb4245b592936
muhamadahfadz <muhamadahfadz@gmail.com>
2022-10-03 11:15:52 +0700

    first commit

---
+++ application/views/input_data.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Input Data Mahasiswa</title>
+</head>
+<body>
+	<center>
+		<h1>Input Data Mahasiswa</h1>
+	</center>
+	<form action="<?php echo base_url(). 'index.php/kampus/tambah_aksi'; ?>" method="POST" enctype="multipart/form-data">
+		<table style="margin:20px auto;">
+			<tr>
+				<td>NIM</td>
+				<td><input type="text" name="nim"></td>
+			</tr>
+			<tr>
+				<td>Nama</td>
+				<td><input type="text" name="nama"></td>
+			</tr>
+			<tr>
+				<td>Alamat</td>
+				<td><input type="text" name="alamat"></td>
+			</tr>
+			<tr>
+				<td>Pekerjaan</td>
+				<td><input type="text" name="pekerjaan"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" value="Tambah"> <input type="reset" value="Batal"> <?php echo anchor('kampus/index','<input type=button value=Kembali>'); ?></td>
+			</tr>
+		</table>
+</body>
+</html>
---
+++ application/views/tampil_data.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Tampil Data</title>
+</head>
+<body>
+	<center><h1>Data Mahasiswa</h1></center>
+	<table style="margin:20px auto;" border="1">
+		<tr>
+			<th>No</th>
+			<th>NIM</th>
+			<th>Nama</th>
+			<th>Alamat</th>
+			<th>Pekerjaan</th>
+			<th>Action</th>
+		</tr>	
+
+		<?php 
+		$no = 1;
+		foreach ($mahasiswa as $u) {
+		 ?>	
+		 <tr>
+		 	<td><?php echo $no++ ?></td>
+		 	<td><?php echo $u->nim ?></td>
+		 	<td><?php echo $u->nama ?></td>
+		 	<td><?php echo $u->alamat ?></td>
+		 	<td><?php echo $u->pekerjaan ?></td>
+		 	<td>
+		 		<?php echo anchor('kampus/edit/'.$u->id,'Edit'); ?>
+		 		<?php echo anchor('kampus/hapus/'.$u->id,'Hapus'); ?>
+		 	</td>
+		 </tr>
+		<?php } ?>
+	</table>
+	<center><?php echo anchor('kampus/tambah','<input type=button value=\'Tambah Data\'>'); ?></center>
+</body>
+</html>
