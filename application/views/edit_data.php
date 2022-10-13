55960ffe16fe7ab6135617da040c3400acac4363
muhamadahfadz <muhamadahfadz@gmail.com>
2022-10-13 09:29:56 +0700

    first commit

--- application/views/edit_data.php
+++ application/views/edit_data.php
@@ -8,7 +8,7 @@
         <h1>Edit Data Mahasiswa</h1>
     </center>
     <?php foreach($mahasiswa as $u) { ?>
-    <form action="<?php echo base_url(). 'index.php/kampus/update'; ?>"method="post" enctype="multipart/form-data">
+    <form action="<?php echo base_url(). 'index.php/kampus/update'; ?>" method="post" enctype="multipart/form-data">
         <table style="margin:20px auto;">
             <tr>
                 <td>NIM</td>
@@ -30,6 +30,10 @@
             <tr>
                 <td>Pekerjaan</td>
                     <td><input type="text" name="pekerjaan" value="<?php echo $u->pekerjaan ?>"></td>
+            </tr>
+            <tr>
+                <td>Upload Foto</td>
+                <td><input type="file" name="foto"></td>
             </tr>
             <tr>
                 <td></td>
